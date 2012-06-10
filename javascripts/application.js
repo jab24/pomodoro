647509a4f320ae7b6dbd50e92a7d87ab47f2f62e
John Borges <jab24@njit.edu>
2012-06-10 17:31:01 -0400

    fixed task bug

--- javascripts/application.js
+++ javascripts/application.js
@@ -4,16 +4,18 @@ jQuery.fn.extend({
 		var elem
 	},
 	resetTaskState:function(){
-		this.resetTaskStateClassNames();
 		return this.each(function(){
-			var element = jQuery(this);
-			var taskStateIndex = element.data('taskStateIndex') || 0;
-			taskStateIndex = (taskStateIndex + 1) % jQuery.fn.taskStates.length;
-			element.data('taskStateIndex',taskStateIndex).addClass(jQuery.fn.taskStates[taskStateIndex]);
-		});
+			jQuery(this).data('taskStateIndex',0).addClass(jQuery.fn.taskStates[0]);
+		)};
 	},
 	toggleTaskState:function(){
-
+		this.resetTaskStateClassNames();
+    return this.each(function(){
+      var element = jQuery(this);
+      var taskStateIndex = element.data('taskStateIndex') || 0;
+      taskStateIndex = (taskStateIndex + 1) % jQuery.fn.taskStates.length;
+      element.data('taskStateIndex',taskStateIndex).addClass(jQuery.fn.taskStates[taskStateIndex]);
+    });	
 	},
 });
 jQuery(function(){
@@ -25,7 +27,10 @@ jQuery(function(){
 
 	$('#add').click(function(){
 		var taskItem = $('#tasks ul li:first').clone();
-		taskItem.find('input[type="text"]').value("");
+		taskItem
+				.find('.completion a').resetTaskState()
+			.end()
+				.find('input[type="text"]').value("");
 		//adds to the end of inside element
 		$('#tasks ul').append(taskItem);
 		taskItem.find('input[type="text"]:first').focus();
