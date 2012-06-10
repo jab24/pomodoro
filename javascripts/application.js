2f2146522f261ae6b849d8496a9f147e7e8f14e0
John Borges <jab24@njit.edu>
2012-06-10 16:28:25 -0400

    added tasks functionality

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,4 +1,14 @@
 jQuery(function(){
-	$('input[type="text"]:first').focus();
 	
+	$('#add').click(function(){
+		var taskItem = $('#tasks ul li:first').clone();
+		taskItem.find('input[type="text"]').value("");
+		//adds to the end of inside element
+		$('#tasks ul').append(taskItem);
+		taskItem.find('input[type="text"]:first').focus();
+		return false;
+	});	
+
+	$('input[type="text"]:first').focus();
+		
 });
