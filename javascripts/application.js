2436665fa2ff7f26748421f4746be36ff7f06238
John Borges <jab24@njit.edu>
2012-06-10 17:21:25 -0400

    added tasks

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,5 +1,28 @@
+jQuery.fn.extend({
+	taskStates:['task-empty','task-x','task-apostrophe','task-dash'],
+	resetTaskStateClassNames:function(){
+		var elem
+	},
+	resetTaskState:function(){
+		this.resetTaskStateClassNames();
+		return this.each(function(){
+			var element = jQuery(this);
+			var taskStateIndex = element.data('taskStateIndex') || 0;
+			taskStateIndex = (taskStateIndex + 1) % jQuery.fn.taskStates.length;
+			element.data('taskStateIndex',taskStateIndex).addClass(jQuery.fn.taskStates[taskStateIndex]);
+		});
+	},
+	toggleTaskState:function(){
+
+	},
+});
 jQuery(function(){
 	
+	$('.completion a').live("click",function(){
+		$(this).toggleTaskState();
+		return false;
+	});
+
 	$('#add').click(function(){
 		var taskItem = $('#tasks ul li:first').clone();
 		taskItem.find('input[type="text"]').value("");
